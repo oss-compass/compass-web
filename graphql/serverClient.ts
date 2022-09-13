01ec2eecd8566f78acf6ab5e52a608c085ffbb4f
neil <msg@nancode.cn>
2022-09-13 16:16:08 +0800

    feat: modify api url

--- graphql/serverClient.ts
+++ graphql/serverClient.ts
@@ -1,11 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 import { GetServerSidePropsContext } from 'next';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-const gqlClient = new GraphQLClient(
-  isDevelopment ? '/api/graphql' : process.env.NEXT_PUBLIC_GRAPHQL_API_URL || ''
-);
+const gqlClient = new GraphQLClient('/api/graphql');
 
 const serverGqlClient = (req: GetServerSidePropsContext['req']) => {
   gqlClient.setHeader('Cookie', req.headers.cookie || '');
