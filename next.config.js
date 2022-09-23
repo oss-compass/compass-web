291f14a259d12c19337775b7e35cb6fcd566632e
neil <msg@nancode.cn>
2022-09-23 13:07:53 +0800

    feat: proxy api in dev

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,6 +12,22 @@ const nextConfig = {
   output: 'standalone',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['portrait.gitee.com'],
+  },
+  async rewrites() {
+    return [
+      isDevelopment
+        ? {
+            source: '/api/graphql',
+            destination: `/api/proxy`,
+          }
+        : {
+            source: '/api/graphql',
+            destination: `${process.env.API_URL}/api/graphql`,
+          },
+    ];
   },
   webpack(config) {
     config.module.rules.push({
