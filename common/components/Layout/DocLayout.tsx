8c560aa8825ffdc3bb9e26df35b4e4316b32b468
neil <msg@nancode.cn>
2022-10-18 18:48:55 +0800

    feat: add mdx plugin

---
+++ common/components/Layout/DocLayout.tsx
@@ -0,0 +1,23 @@
+import React, { PropsWithChildren } from 'react';
+import { Header, Center } from 'common/components/Layout';
+import FooterLinks from 'common/components/FooterLinks';
+import Copyright from 'common/components/Copyright';
+
+const DocLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="flex h-full flex-col">
+      <Header contentCenter />
+      <main className="mx-auto w-[1200px] flex-1 py-10 md:w-full">
+        {children}
+      </main>
+      <footer>
+        <Center>
+          <FooterLinks />
+        </Center>
+        <Copyright />
+      </footer>
+    </div>
+  );
+};
+
+export default DocLayout;
--- common/components/Layout/index.tsx
+++ common/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 export { default as Center } from './Center';
 export { default as Content } from './Content';
+export { default as DocLayout } from './DocLayout';
 export { default as Header } from './Header';
 export { default as Main } from './Main';
 export { default as MobileHeader } from './MobileHeader';
