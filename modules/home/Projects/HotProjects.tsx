8617661fa07371648a20be14ca0d0a8b04efa0bf
coder-sett <49544067+coder-sett@users.noreply.github.com>
2022-11-01 14:55:48 +0800

    feat:add markingsystem description and minichart (#3)
    Signed-off-by: coder-sett <49544067+coder-sett@users.noreply.github.com>

--- modules/home/Projects/HotProjects.tsx
+++ modules/home/Projects/HotProjects.tsx
@@ -6,6 +6,10 @@ import { numberFormatK } from '@common/utils';
 import { gsap } from 'gsap';
 import { useInterval } from 'ahooks';
 import { getAnalyzeLink } from '@common/utils';
+import MiniChart from './MiniChart';
+import AngleL from './assets/angle-left.svg';
+import AngleR from './assets/angle-right.svg';
+import { transMarkingSystem } from '@modules/analyze/DataTransform/transMarkingSystem';
 
 const getLink = (
   path: string | null | undefined,
@@ -25,7 +29,10 @@ type Repo = NonNullable<OverviewQuery['overview']['trends']>[number];
 
 const Project: React.FC<{ repo: Repo; index: number }> = ({ repo, index }) => {
   const ref = useRef(null);
-
+  const echartsData =
+    repo.metricActivity?.map((item) =>
+      transMarkingSystem(item.activityScore || '')
+    ) || [];
   useEffect(() => {
     gsap.to(ref.current, {
       rotateX: 360,
@@ -49,10 +56,13 @@ const Project: React.FC<{ repo: Repo; index: number }> = ({ repo, index }) => {
           <p className="text-sm text-gray-400 line-clamp-1">{repo.language}</p>
         </a>
       </Link>
-      <p className="text flex items-center">
+      <div className="h-6 w-1/3">
+        <MiniChart echartsData={echartsData} />
+      </div>
+      {/* <p className="text flex items-center">
         <AiOutlineStar className="mr-1 text-gray-600" />
         {numberFormatK(Number(repo.stargazersCount))}
-      </p>
+      </p> */}
     </div>
   );
 };
@@ -79,7 +89,33 @@ const HotProjects: React.FC<{
 
   return (
     <div className="lg:px-4">
-      <div className="mb-6 text-2xl font-bold">Hot Projects</div>
+      <div className="mb-6 flex justify-between text-2xl font-bold">
+        Trends
+        <div className="flex justify-between py-2">
+          <span
+            className="mr-2.5 cursor-pointer"
+            onClick={() => {
+              setIndex((pre) => {
+                if (pre === 1) return Math.floor(total / 6);
+                return pre - 1;
+              });
+            }}
+          >
+            <AngleL />
+          </span>
+          <span
+            className="cursor-pointer"
+            onClick={() => {
+              setIndex((pre) => {
+                if (pre >= Math.floor(total / 6)) return 1;
+                return pre + 1;
+              });
+            }}
+          >
+            <AngleR />
+          </span>
+        </div>
+      </div>
       <div
         className="flex w-[664px] flex-wrap rounded border-t border-l lg:w-full"
         id="HotProjects"
