daad7a85445ae162e9978ff246d8aa80a5b9e167
laixingyou <laixingyou777@gmail.com>
2022-11-10 14:10:47 +0800

    fix:ratio yaxis move left
    Signed-off-by: laixingyou <laixingyou777@gmail.com>

--- modules/analyze/DataView/CodeQuality/PRIssueLinked.tsx
+++ modules/analyze/DataView/CodeQuality/PRIssueLinked.tsx
@@ -11,17 +11,22 @@ import LoadInView from '@modules/analyze/components/LoadInView';
 import Chart from '@modules/analyze/components/Chart';
 import { ChartThemeState } from '@modules/analyze/context';
 import { LineSeriesOption } from 'echarts';
+import { toFixed } from '@common/utils';
 
 const tansOpts: TransOpts = {
   metricType: 'metricCodequality',
   xAxisKey: 'grimoireCreationDate',
   yAxisOpts: [
+    {
+      legendName: 'linked issue ratio',
+      valueKey: 'prIssueLinkedRatio',
+      valueFormat: (v) => toFixed(v * 100, 2),
+    },
     { legendName: 'total pr', valueKey: 'prCount' },
     {
       legendName: 'linked issue',
       valueKey: 'prIssueLinkedCount',
     },
-    { legendName: 'linked issue ratio', valueKey: 'prIssueLinkedRatio' },
   ],
 };
 
@@ -38,23 +43,50 @@ const getOptions = (
         isCompare,
         legendTypeCount: len,
       });
+      console.log(yResults);
+
       if (legendName === 'linked issue ratio') {
-        return line({ name, data, color, yAxisIndex: 1 });
+        return line({ name, data, color, yAxisIndex: 0 });
       }
-      return line({ name, data, color });
+      return line({ name, data, color, yAxisIndex: 1 });
     },
     theme
   );
   return getLineOption({
     xAxisData: xAxis,
     series,
-    yAxis: [{ type: 'value' }, { type: 'value' }],
+    yAxis: [
+      { type: 'value', axisLabel: { formatter: '{value}%' } },
+      { type: 'value' },
+    ],
     tooltip: {
       trigger: 'axis',
       axisPointer: {
         type: 'cross',
       },
       order: 'seriesDesc',
+      formatter: function (params: any) {
+        if (!params) return '';
+        let label = '';
+        let tpl = params
+          .map((param: any) => {
+            label = param.axisValueLabel;
+            let data = param.data === null ? '-' : param.data;
+            if (param.seriesName.indexOf('ratio') > -1 && data !== '-') {
+              data += '%';
+            }
+            return `<div style="margin: 5px 0 5px;line-height:1;">${param.marker}
+<span style="font-size:14px;color:#666;font-weight:400;margin-left:2px">${param.seriesName}</span>
+<span style="float:right;margin-left:20px;font-size:14px;color:#666;font-weight:900">${data}</span>
+</div>`;
+          })
+          .join('');
+
+        return (
+          `<div style="font-size:14px;color:#666;font-weight:400;line-height:1;">${label}</div>` +
+          tpl
+        );
+      },
     },
   });
 };
