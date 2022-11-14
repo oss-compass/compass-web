cd480939e88f43cdcf34709ddc6a1dfc1644122d
neil <msg@nancode.cn>
2022-11-14 15:35:17 +0800

    ci: add github action yml
    Signed-off-by: neil <msg@nancode.cn>

--- common/utils/time.test.ts
+++ common/utils/time.test.ts
@@ -1,11 +1,19 @@
 import { formatToNow, formatISO } from './time';
 
 describe('time', () => {
+  beforeAll(() => {
+    jest.useFakeTimers({ now: new Date('2022-11-14T14:33:00') });
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it('formatISO', () => {
     expect(formatISO('2022-09-28T02:11:54Z')).toEqual('2022-09-28');
   });
 
   it('formatToNow', function () {
-    expect(formatToNow('2022-09-28T02:11:54Z')).toBe('1 month');
+    expect(formatToNow('2022-09-28T02:11:54Z')).toBe('2 months');
   });
 });
--- modules/analyze/options/color.ts
+++ modules/analyze/options/color.ts
@@ -12,25 +12,3 @@ export const getPalette = (index: number) => {
 export const getPaletteColor = (palette: string[], index: number) => {
   return palette[index % palette.length];
 };
-
-/**
- * @deprecated
- */
-export const colorGenerator = () => {
-  let labelIndex = 0;
-  const labelMap: Record<string, { paletteIndex: number; count: number }> = {};
-
-  return (label: string) => {
-    if (!labelMap[label]) {
-      const palette = getPalette(labelIndex);
-      labelMap[label] = { paletteIndex: labelIndex, count: DefaultIndex };
-      labelIndex++;
-      return palette[DefaultIndex];
-    }
-
-    labelMap[label].count++;
-    const { paletteIndex, count } = labelMap[label];
-    const palette = palettes[paletteIndex];
-    return palette[count % palette.length];
-  };
-};
