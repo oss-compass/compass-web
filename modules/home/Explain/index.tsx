d4d174536aa19ed4063fc303518a326b76516333
laixingyou <1156663717@qq.com>
2022-11-17 15:16:49 +0800

    feat: add plant map (#14)
    * feat: add plant map
    
    Signed-off-by: laixingyou <laixingyou777@gmail.com>

--- modules/home/Explain/index.tsx
+++ modules/home/Explain/index.tsx
@@ -1,33 +1,31 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import classnames from 'classnames';
 import Link from 'next/link';
 import { useCounter } from 'react-use';
 import { AiOutlineArrowRight } from 'react-icons/ai';
-import Planet01 from './assets/planet-01.svg';
-import Planet02 from './assets/planet-02.svg';
-import Planet03 from './assets/planet-03.svg';
+import { plantList, PopContent } from './plantConfig';
 
 import styles from './index.module.scss';
 
 const PopCard: React.FC<{
   className?: string;
+  popContent: PopContent;
   onNext: () => void;
-}> = ({ className, onNext }) => {
+}> = ({ className, popContent, onNext }) => {
   return (
     <div
       className={classnames(
         styles.popCardBg,
-        'absolute h-[141px] w-[304px] rounded p-4 opacity-0 transition-all',
+        'absolute h-[141px] w-[304px] rounded p-4 transition-all',
         className
       )}
     >
-      <h2 className="text-white">Commit frequency</h2>
-      <p className="mb-4 text-xs text-gray-400">
-        The growth in the aggregated count of unique contributors analyzed
-        during the selected time period.
+      <h2 className="text-white">{popContent.title}</h2>
+      <p className="mb-4 max-h-[60px] break-words text-xs text-gray-400 line-clamp-4">
+        {popContent.content}
       </p>
       <div className="flex justify-between">
-        <Link href="/">
+        <Link href={popContent.hash}>
           <a className="cursor-pointer text-xs text-[#3A5BEF]">Know more</a>
         </Link>
         <div
@@ -43,82 +41,78 @@ const PopCard: React.FC<{
   );
 };
 
-const MaskPlanet: React.FC<{ className?: string; enable: boolean }> = ({
-  enable,
-  className,
-}) => {
+const Plant: React.FC<
+  PropsWithChildren<{
+    prop: { top: number; left: number; size: number; color: string };
+    className: string;
+    onMouseHover: () => void;
+  }>
+> = ({ children, prop, className, onMouseHover }) => {
   return (
     <div
+      style={{
+        top: prop.top + 'px',
+        left: prop.left + 'px',
+        width: prop.size + 'px',
+        height: prop.size + 'px',
+      }}
       className={classnames(
-        'absolute rounded-full bg-black opacity-50 transition-all duration-150',
-        className,
-        { '!opacity-0': enable }
+        styles.box,
+        styles[prop.color],
+        'absolute md:hidden',
+        className
       )}
-    ></div>
+    >
+      <div
+        className={styles.boxHover}
+        onMouseEnter={() => {
+          onMouseHover();
+        }}
+      ></div>
+      {children}
+    </div>
   );
 };
-
 const SectionExplain = () => {
-  const [value, { inc, reset }] = useCounter(1, 3, 1);
-
+  const [value, { inc, reset, set }] = useCounter(0, plantList.length, 0);
   return (
     <section>
-      <div className={classnames(styles.bg, 'h-[900px] w-full')}>
-        <div className="relative mx-auto w-[1200px] lg:w-full">
+      <div
+        className={classnames(
+          styles.bg,
+          'relative h-[900px] w-full overflow-hidden'
+        )}
+      >
+        <div className="mx-auto w-[1200px] lg:w-full">
           <h1 className="absolute top-16 text-6xl text-white md:px-2 md:text-3xl">
             How Compass working
           </h1>
-
-          <div className="absolute top-[400px] left-[160px] cursor-pointer md:hidden">
-            <Planet01 />
-            <MaskPlanet
-              enable={value === 1}
-              className="top-[8px] left-[8px] h-[57px] w-[57px]"
-            />
-            <PopCard
-              className={classnames('left-[45px] top-[45px]', {
-                '!opacity-100': value === 1,
-                // '!left-10': value === 1,
-              })}
-              onNext={() => {
-                inc();
-              }}
-            />
-          </div>
-
-          <div className="absolute top-[200px] right-[160px] cursor-pointer md:hidden">
-            <Planet02 />
-            <MaskPlanet
-              enable={value === 2}
-              className="top-[10px] left-[10px] h-[48px] w-[48px]"
-            />
-            <PopCard
-              className={classnames('right-[45px] top-[45px]', {
-                '!opacity-100': value === 2,
-                // '!right-10': value === 2,
-              })}
-              onNext={() => {
-                inc();
-              }}
-            />
-          </div>
-
-          <div className="absolute top-[500px] right-[180px] cursor-pointer md:hidden">
-            <Planet03 />
-            <MaskPlanet
-              enable={value === 3}
-              className="top-[6px] left-[5px] h-[51px] w-[51px]"
-            />
-            <PopCard
-              className={classnames('right-[45px] top-[45px]', {
-                '!opacity-100': value === 3,
-                // '!top-10': value === 3,
-              })}
-              onNext={() => {
-                reset();
-              }}
-            />
-          </div>
+        </div>
+        <div className="relative top-1/2 left-1/2">
+          {plantList.map(({ popContent, ...item }, i) => {
+            return (
+              <Plant
+                key={i}
+                prop={item}
+                onMouseHover={() => {
+                  set(i);
+                }}
+                className={classnames({ '!opacity-100': value == i })}
+              >
+                <PopCard
+                  className={classnames('left-[25px]', {
+                    invisible: value !== i,
+                    '-top-[145px]': item.bottom,
+                    '-left-[245px]': item.right,
+                  })}
+                  popContent={popContent}
+                  onNext={() => {
+                    i === plantList.length - 1 ? reset() : inc();
+                  }}
+                />
+              </Plant>
+            );
+          })}
         </div>
       </div>
     </section>
