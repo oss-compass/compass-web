293bc8ef5ed31c50d24572d92f5881776a8ac8f4
neil <msg@nancode.cn>
2022-12-12 10:20:03 +0800

    fix: change local cookie key
    Signed-off-by: neil <msg@nancode.cn>

--- common/utils/getLocale.ts
+++ common/utils/getLocale.ts
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 type TypeLang = 'zh' | 'en';
 
 const locales = ['zh', 'en'];
-const LOCALE_KEY = 'user_locale';
+export const USER_LOCALE_KEY = 'locale';
 
 function getLocale(): TypeLang;
 
@@ -12,10 +12,10 @@ function getLocale(reqCookies: NextApiRequestCookies): TypeLang;
 
 function getLocale(reqCookies?: NextApiRequestCookies | undefined): TypeLang {
   if (reqCookies) {
-    const language = reqCookies[LOCALE_KEY] as TypeLang;
+    const language = reqCookies[USER_LOCALE_KEY] as TypeLang;
     return locales.includes(language) ? language : 'en';
   } else if (typeof reqCookies === 'undefined') {
-    return (Cookies.get(LOCALE_KEY) as TypeLang) || 'en';
+    return (Cookies.get(USER_LOCALE_KEY) as TypeLang) || 'en';
   }
   return 'en';
 }
