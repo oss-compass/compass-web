4950a48ec156f97f2e4846cbd57966140a48a93c
neil <msg@nancode.cn>
2023-02-18 13:16:12 +0800

    feat: add verify yml content logic
    Signed-off-by: neil <msg@nancode.cn>

--- script/gen_collections_file.ts
+++ script/gen_collections_file.ts
@@ -2,21 +2,26 @@ import { resolve } from 'node:path';
 import fs from 'fs-extra';
 import simpleGit from 'simple-git';
 import yaml from 'yaml';
+import Joi from 'joi';
 
 const git = simpleGit();
 
-const repoPath = './script/tmp/compass-projects-information';
-const collectionsDir = './script/tmp/compass-projects-information/collections';
 const gitAddress =
-  'https://github.com/EdmondFrank/compass-projects-information.git';
+  'https://github.com/oss-compass/compass-projects-information.git';
+const repoClonePath = './script/tmp/compass-projects-information';
+const collectionsDir = './script/tmp/compass-projects-information/collections';
 
 async function handle() {
   console.log('start generate collections json...');
 
-  await fs.remove(repoPath);
-  const res = await git.clone(gitAddress, repoPath);
+  await fs.remove(repoClonePath);
+  const res = await git.clone(gitAddress, repoClonePath);
   console.log(res);
-  const files = await fs.readdir(collectionsDir);
+
+  const files = (await fs.readdir(collectionsDir)).filter((f) =>
+    f.endsWith('.yml')
+  );
+  console.log(files);
   const collectionsData: Record<string, object> = {};
 
   for (const f of files) {
@@ -26,14 +31,42 @@ async function handle() {
     );
     const ymlContent = await fs.readFile(p, { encoding: 'utf-8' });
     const jsonContent = yaml.parse(ymlContent);
+
     const ident = jsonContent.ident;
     collectionsData[ident] = jsonContent;
   }
 
+  validatorData(collectionsData);
   await fs.writeJSON('./script/tmp/collections.json', collectionsData);
 
   console.log('collections.json generate success!');
   console.log();
+}
+
+function validatorData(data: any) {
+  const CollectionSchema = Joi.object({
+    ident: Joi.string().min(1),
+    name: Joi.string().min(1),
+    name_cn: Joi.string().min(1),
+    slug: Joi.string().min(1).pattern(/^\/.+/),
+    items: Joi.array()
+      .unique()
+      .items(
+        Joi.string().pattern(/^https?:\/\/(www\.)?(github\.com|gitee\.com).+/)
+      ),
+  });
+
+  const keys = Object.keys(data);
+  keys.some((key) => {
+    const obj = data[key];
+    const { error } = CollectionSchema.validate(obj);
+    if (error) {
+      throw error;
+    }
+    return false;
+  });
+
+  console.log('validate success！！！');
 }
 
 handle().catch((e) => {
