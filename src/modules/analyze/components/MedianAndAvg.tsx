fbdde735289e399eb7f18d5976c5b8f324dfebb6
laixingyou <laixy7@gmail.com>
2023-04-18 20:38:05 +0800

    feat:Optimize report page filter option
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- src/modules/analyze/components/MedianAndAvg.tsx
+++ src/modules/analyze/components/MedianAndAvg.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import Average from 'public/images/analyze/average.svg';
 import Median from 'public/images/analyze/median.svg';
 import { useTranslation } from 'next-i18next';
-import Tooltip from '@common/components/Tooltip';
 import classnames from 'classnames';
 import { subscribeKey } from 'valtio/utils';
 import { avgAndScoreState } from '@modules/analyze/store';
+import Svg100 from 'public/images/analyze/number-100.svg';
+import Svg1 from 'public/images/analyze/number-1.svg';
 
 const MedianAndAvg: React.FC<{
   showAvg: boolean;
   onAvgChange: (pre: boolean) => void;
   showMedian: boolean;
   onMedianChange: (pre: boolean) => void;
-}> = ({ showAvg, onAvgChange, showMedian, onMedianChange }) => {
+  onePoint: boolean;
+  onChange: (pre: boolean) => void;
+}> = ({
+  showAvg,
+  onAvgChange,
+  showMedian,
+  onMedianChange,
+  onePoint,
+  onChange,
+}) => {
   const { t } = useTranslation();
   subscribeKey(avgAndScoreState, 'showAvg', (v) => {
     if (showAvg !== v) {
@@ -26,44 +36,59 @@ const MedianAndAvg: React.FC<{
   });
   return (
     <>
-      <Tooltip
-        arrow
-        title={
-          showAvg ? t('analyze:avg_line.hide') : t('analyze:avg_line.show')
-        }
+      <div className="border-b py-2 pl-3.5 font-bold text-gray-900">
+        {t('analyze:display')}
+      </div>
+      <div
+        className={classnames(
+          'group flex cursor-pointer border-b py-2 pl-3.5 transition',
+          { 'text-primary': showAvg }
+        )}
+        onClick={() => {
+          onAvgChange(!showAvg);
+        }}
       >
-        <div
-          className={classnames(
-            'group ml-2 cursor-pointer p-1 transition md:hidden',
-            { 'opacity-60': !showAvg }
-          )}
-          onClick={() => {
-            onAvgChange(!showAvg);
-          }}
-        >
-          <Average />
-        </div>
-      </Tooltip>
-      <Tooltip
-        arrow
-        title={
-          showMedian
-            ? t('analyze:median_line.hide')
-            : t('analyze:median_line.show')
-        }
+        <Average className="mr-2" />
+        {t('analyze:avg_line.show')}
+      </div>
+      <div
+        className={classnames(
+          'group flex cursor-pointer border-b py-2 pl-3.5 transition',
+          { 'text-primary': showMedian }
+        )}
+        onClick={() => {
+          onMedianChange(!showMedian);
+        }}
       >
-        <div
-          className={classnames(
-            'group ml-2 cursor-pointer p-1 transition md:hidden',
-            { 'opacity-60': !showMedian }
-          )}
-          onClick={() => {
-            onMedianChange(!showMedian);
-          }}
-        >
-          <Median />
-        </div>
-      </Tooltip>
+        <Median className="mr-2" />
+        {t('analyze:median_line.show')}
+      </div>
+      <div
+        className={classnames(
+          'group flex cursor-pointer py-2 pl-3.5 transition',
+          { 'text-primary': !onePoint }
+        )}
+        data-html2canvas-ignore="true"
+        onClick={() => {
+          onChange(false);
+        }}
+      >
+        <Svg100 className="mr-2" />
+        <span>{t('analyze:mark.percentage')}</span>
+      </div>
+      <div
+        className={classnames(
+          'group flex cursor-pointer border-b py-2 pl-3.5 transition',
+          { 'text-primary': onePoint }
+        )}
+        data-html2canvas-ignore="true"
+        onClick={() => {
+          onChange(true);
+        }}
+      >
+        <Svg1 className="mr-2" />
+        <span>{t('analyze:mark.point')}</span>
+      </div>
     </>
   );
 };
---
+++ src/modules/analyze/components/NavBar/NavbarSetting.tsx
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+import ScoreConversion from '@modules/analyze/components/ScoreConversion';
+import MedianAndAvg from '@modules/analyze/components/MedianAndAvg';
+import { useSnapshot } from 'valtio';
+import { avgAndScoreState } from '@modules/analyze/store';
+import { AiOutlineSetting } from 'react-icons/ai';
+import { useClickAway, useToggle } from 'react-use';
+import classnames from 'classnames';
+
+const NavbarSetting: React.FC = () => {
+  const snap = useSnapshot(avgAndScoreState);
+  const [dropdownOpen, toggleDropdown] = useToggle(false);
+  const ref = useRef(null);
+  useClickAway(ref, () => {
+    toggleDropdown(false);
+  });
+  return (
+    <div className="relative">
+      <div
+        className="mx-4 flex h-8 w-8 items-center justify-center rounded-full border border-[#CFCFCF]"
+        onClick={() => toggleDropdown()}
+      >
+        <AiOutlineSetting className="text-xl" />
+      </div>
+      <ul
+        ref={ref}
+        style={{ boxShadow: '0px 1px 4px 1px rgba(0,0,0,0.1)' }}
+        className={classnames(
+          'absolute right-0 z-[200] w-[150px] rounded bg-base-100 py-2 text-xs',
+          { hidden: !dropdownOpen }
+        )}
+      >
+        <MedianAndAvg
+          showAvg={snap.showAvg}
+          onAvgChange={(b) => (avgAndScoreState.showAvg = b)}
+          showMedian={snap.showMedian}
+          onMedianChange={(b) => (avgAndScoreState.showMedian = b)}
+          onePoint={snap.onePointSys}
+          onChange={(v) => {
+            avgAndScoreState.onePointSys = v;
+          }}
+        />
+      </ul>
+    </div>
+  );
+};
+
+export default NavbarSetting;
