e477961b9bd1f6bc08c7b4abbe16ebb5a9591672
laixingyou <laixy7@gmail.com>
2023-05-11 16:03:03 +0800

    feat: login page
    Signed-off-by: laixingyou <laixy7@gmail.com>

---
+++ src/modules/auth/components/LogoHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '@public/images/logos/compass-logo.svg';
+
+const LogoHeader = () => {
+  return (
+    <div className="flex h-20 items-center justify-center border-0 border-b border-solid border-[#CFCFCF]">
+      <Logo />
+    </div>
+  );
+};
+
+export default LogoHeader;
