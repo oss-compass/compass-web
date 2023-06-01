f2811abfa05a780bafc9ddd9f13bb01edfdded02
neil <msg@nancode.cn>
2023-06-01 17:48:54 +0800

    feat: not cache closed state
    Signed-off-by: neil <msg@nancode.cn>

--- src/common/components/Questionnaire.tsx
+++ src/common/components/Questionnaire.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useLocalStorageState } from 'ahooks';
 import { useTranslation } from 'react-i18next';
 
 const Questionnaire = () => {
   const { i18n } = useTranslation();
-  const [close, setClose] = useLocalStorageState<boolean>(
-    'questionnaire' + '/s2/12430808/ef24/',
-    {
-      defaultValue: false,
-    }
-  );
+  const [close, setClose] = useState(false);
 
   if (!close && i18n.language === 'zh') {
     return (
