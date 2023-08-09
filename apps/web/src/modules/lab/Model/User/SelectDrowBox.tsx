c7dfce44653caa929e6288c188e3e707920c26d8
laixingyou <laixy7@gmail.com>
2023-08-09 16:06:59 +0800

    feat: add member management and i18n (#196)
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- apps/web/src/modules/lab/Model/User/SelectDrowBox.tsx
+++ apps/web/src/modules/lab/Model/User/SelectDrowBox.tsx
@@ -1,20 +1,21 @@
 import React, { PropsWithChildren, useState, useRef } from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import { CustomRadio, Select, SelectOption, Input } from '@oss-compass/ui';
 import classNames from 'classnames';
 import { useClickAway } from 'react-use';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { useTranslation } from 'react-i18next';
 
 const SelectDrowBox: React.FC<
   PropsWithChildren<{
-    rolesList?: string[];
-    options?: { name: string; desc: string }[];
-    cls?: string;
-    onChange?: (value: string) => void;
+    roles: {
+      canExecute: boolean;
+      canUpdate: boolean;
+    };
+    options?: { name: string; desc: string; id: string; disable?: boolean }[];
+    onChange?: (id: string) => void;
     onShowDrowBox?: (value: boolean) => void;
   }>
-> = ({ rolesList, options, cls, onChange, onShowDrowBox, children }) => {
+> = ({ roles, options, onChange, onShowDrowBox, children }) => {
+  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   useClickAway(ref, () => {
@@ -34,35 +35,48 @@ const SelectDrowBox: React.FC<
       </div>
       <div
         className={classNames(
-          'absolute top-11 w-52 overflow-auto border border-solid border-slate-200 bg-white pt-1 text-sm text-slate-900  shadow  shadow-slate-200 outline-0',
+          'absolute w-52 overflow-auto border border-solid border-slate-200 bg-white pt-1 text-sm text-slate-900  shadow  shadow-slate-200 outline-0',
           { hidden: !open }
         )}
       >
-        <div className="h-6 py-1 pl-[14px] text-xs text-[#868690]">可多选</div>
-        {options.map(({ name, desc }) => {
-          return (
-            <div
-              onClick={() => {
-                onChange(name);
-              }}
-              key={name}
-              className="flex h-[60px]  cursor-pointer border-t border-[#eff0f0] py-3 pl-[14px] hover:bg-[#E7E7E7]"
-            >
+        <div className="h-6 py-1 pl-[14px] text-xs text-[#868690]">
+          {t('lab:user.multiple')}
+        </div>
+        {options.map(({ name, desc, id, disable }) => {
+          if (disable) {
+            return (
               <div
-                className={classNames('mt-0.5 mr-2 flex ', [
-                  rolesList.includes(name)
-                    ? 'text-[#3a5bef]'
-                    : 'text-[#939393]',
-                ])}
+                key={id}
+                className="flex cursor-not-allowed border-t border-[#eff0f0] py-3 pl-[14px] hover:bg-[#E7E7E7]"
               >
-                <BsFillCheckCircleFill />
+                <div className="mt-0.5 mr-2 flex w-[14px] flex-shrink-0 text-[#939393]">
+                  <BsFillCheckCircleFill />
+                </div>
+                <div className="flex flex-col">
+                  <div className="font-medium text-[#000000]">{name}</div>
+                  <div className="mt-1 text-xs text-[#868690]">{desc}</div>
+                </div>
               </div>
-              <div className="flex flex-col">
-                <div className="font-medium text-[#000000]">{name}</div>
-                <div className="mt-1 text-xs text-[#868690]">{desc}</div>
+            );
+          } else {
+            return (
+              <div
+                onClick={() => {
+                  onChange(id);
+                }}
+                key={id}
+                className="flex cursor-pointer border-t border-[#eff0f0] py-3 pl-[14px] hover:bg-[#E7E7E7]"
+              >
+                <div className="mt-0.5 mr-2 flex w-[14px] flex-shrink-0 text-[#3a5bef]">
+                  {roles[id] && <BsFillCheckCircleFill />}
+                </div>
+                <div className="flex flex-col">
+                  <div className="font-medium text-[#000000]">{name}</div>
+                  <div className="mt-1 text-xs text-[#868690]">{desc}</div>
+                </div>
               </div>
-            </div>
-          );
+            );
+          }
         })}
       </div>
     </div>
