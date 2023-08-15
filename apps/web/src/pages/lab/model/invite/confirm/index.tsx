543772e1df9747c2469b2721bbb7021b7b1234d0
laixingyou <laixy7@gmail.com>
2023-08-15 14:16:23 +0800

    feat:add invite confirm and lab terms (#198)
    Signed-off-by: laixingyou <laixy7@gmail.com>

---
+++ apps/web/src/pages/lab/model/invite/confirm/index.tsx
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import Router, { useRouter } from 'next/router';
+import Copyright from '@modules/auth/components/Copyright';
+import LogoHeader from '@modules/auth/components/LogoHeader';
+import AuthRequire from '@modules/auth/AuthRequire';
+import { Button } from '@oss-compass/ui';
+import Mail from '@public/images/login/mail.svg';
+import getLocalesFile from '@common/utils/getLocalesFile';
+import { GetServerSidePropsContext } from 'next';
+import { useTranslation, Trans } from 'next-i18next';
+import { toast } from 'react-hot-toast';
+import Dialog from '@common/components/Dialog';
+import LabTerms from '@modules/lab/components/LabTerms';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+  return {
+    props: {
+      ...(await getLocalesFile(req.cookies, ['lab'])),
+    },
+  };
+}
+
+const Content = ({ status }: { status: string }) => {
+  const { t } = useTranslation();
+  const router = useRouter();
+  const acceptUrl = router.query.accept_url as string;
+  const invitee = router.query.invitee as string;
+  const modelName = router.query.model as string;
+
+  const [select, setSelect] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="flex pl-[56px] pr-[44px] pt-[64px]">
+        <div
+          className={classNames(
+            'bg-primary flex h-24 w-24 flex-shrink-0 items-center justify-center rounded-full'
+          )}
+        >
+          <Mail />
+        </div>
+        <div className="pl-6">
+          <h1 className="m-0 mb-2 text-xl font-semibold text-black sm:leading-8">
+            {t('lab:joining_the_compass')}
+          </h1>
+          <div className="text-sm text-black">
+            <>
+              <Trans
+                i18nKey="you_are_here_because_you_have_been_invited"
+                ns="lab"
+                values={{ invitee: invitee, modelName: modelName }}
+                components={{
+                  s: <span className="font-semibold" />,
+                }}
+              />
+            </>
+          </div>
+          <div className="mt-6 text-sm text-black">
+            <input
+              className="mr-2"
+              checked={select}
+              type="checkbox"
+              onChange={() => {
+                setSelect(!select);
+              }}
+            />
+            {t('lab:i_have_understood_and_agreed_to_the')}{' '}
+            <span
+              className="cursor-pointer underline"
+              onClick={() => {
+                setOpen(true);
+              }}
+            >
+              {t('lab:experimental_model_terms_of_use')}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 mb-[56px] ml-[190px]">
+        <Button
+          size="lg"
+          onClick={() => {
+            if (select) {
+              router.push(acceptUrl);
+            } else {
+              toast.error(t('lab:please_check_the_terms'));
+            }
+          }}
+        >
+          {t('lab:agree_to_join')}
+        </Button>
+      </div>
+      <Dialog
+        open={open}
+        maxWidth={'xl'}
+        dialogContent={<LabTerms />}
+        dialogActions={
+          <>
+            <Button
+              intent="text"
+              size="sm"
+              className="mr-2"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              {t('common:btn.confirm')}
+            </Button>
+          </>
+        }
+        handleClose={() => {
+          setOpen(false);
+        }}
+      />
+    </>
+  );
+};
+
+const Confirm: React.FC = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+  const status = router.query.status as string;
+
+  return (
+    <div>
+      <LogoHeader />
+      <div className="flex justify-center">
+        <div
+          className={classNames(
+            'mt-36 w-[740px] max-w-full border border-solid border-[#CFCFCF] bg-white text-base text-slate-700 shadow-sm',
+            'md:mx-6 md:w-full md:px-6'
+          )}
+        >
+          <AuthRequire className="p-6">
+            <Content status={status} />
+          </AuthRequire>
+        </div>
+      </div>
+      <Copyright />
+    </div>
+  );
+};
+
+export default Confirm;
