5e15f69a9535ae1ea75caf136195fdac830f012f
laixingyou <laixy7@gmail.com>
2023-10-26 10:01:02 +0800

    feat:hidden Event Preview (#254)
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- apps/web/src/modules/explore/RepoCard.tsx
+++ apps/web/src/modules/explore/RepoCard.tsx
@@ -54,7 +54,9 @@ const RepoCard = (props: {
               {compareMode ? (
                 repo
               ) : (
-                <Link href={getShortAnalyzeLink(shortCode)}>{repo}</Link>
+                <Link href={getShortAnalyzeLink(shortCode)} target="_blank">
+                  {repo}
+                </Link>
               )}
             </p>
             <p className="h-6 truncate text-sm text-gray-400">{nameSpace}</p>
