a9378af2e89c5837afa7fd86572000898c265e35
laixingyou <laixy7@gmail.com>
2024-02-26 19:40:45 +0800

    feat: add contributor hub url contributor search (#321)
    Signed-off-by: laixingyou <laixy7@gmail.com>

---
+++ apps/web/src/modules/analyze/DataView/MetricDetail/MetricContributor/ContributorTable/ContributorDropdown.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+// import { AiOutlineSearch } from 'react-icons/ai';
+import { Button, Input } from 'antd';
+import { useTranslation } from 'next-i18next';
+
+const ContributorDropdown = ({ selectedKeys, setSelectedKeys, confirm }) => {
+  const { t } = useTranslation();
+  const [contributor, setContributor] = useState(selectedKeys);
+
+  const handleSearch = () => {
+    setSelectedKeys(contributor);
+    confirm();
+  };
+  const handleReset = () => {
+    setContributor([]);
+  };
+  return (
+    <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+      <Input
+        placeholder={t('analyze:metric_detail:search_contributor')}
+        value={contributor[0]}
+        onChange={(e) => setContributor([e.target.value])}
+        onPressEnter={() => handleSearch()}
+        style={{ marginBottom: 8, display: 'block' }}
+      />
+      <div className="flex justify-between">
+        <Button
+          type="link"
+          disabled={contributor[0] ? false : true}
+          onClick={() => handleReset()}
+          size="small"
+        >
+          {t('analyze:metric_detail:reset')}
+        </Button>
+        <Button
+          type="primary"
+          className="flex items-center"
+          onClick={() => handleSearch()}
+          size="small"
+        >
+          {t('analyze:metric_detail:search')}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ContributorDropdown;
--- apps/web/src/modules/analyze/DataView/MetricDetail/MetricContributor/ContributorTable/index.tsx
+++ apps/web/src/modules/analyze/DataView/MetricDetail/MetricContributor/ContributorTable/index.tsx
@@ -17,17 +17,19 @@ import { getMaxDomain } from '../utils';
 import { getContributorPolling, getContributorExport } from './tableDownload';
 import DomainPersona from './DomainPersona';
 import ContributorName from './ContributorName';
+import ContributorDropdown from './ContributorDropdown';
 import { useTranslation } from 'next-i18next';
 import Download from '@common/components/Table/Download';
 import { useRouter } from 'next/router';
 import { useHandleQueryParams } from '@modules/analyze/hooks/useHandleQueryParams';
 import Dialog from '@common/components/Dialog';
 import Tooltip from '@common/components/Tooltip';
-import { FiEdit } from 'react-icons/fi';
-import { GrClose } from 'react-icons/gr';
 import ManageOrgEdit from '@common/components/OrgEdit/ManageOrgEdit';
 import useVerifyDetailRange from '@modules/analyze/hooks/useVerifyDetailRange';
 import { useIsCurrentUser } from '@modules/analyze/hooks/useIsCurrentUser';
+import { FiEdit } from 'react-icons/fi';
+import { GrClose } from 'react-icons/gr';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 import type { ColumnsType, TablePaginationConfig } from 'antd/es/table';
 import type { FilterValue, SorterResult } from 'antd/es/table/interface';
@@ -90,13 +92,12 @@ const MetricTable: React.FC<{
         return `${t('analyze:total_people', { total })} `;
       },
     },
-    filterOpts: filterOpts,
     sortOpts: defaultSortOpts,
   });
   const query = {
     page: tableParams.pagination.current,
     per: tableParams.pagination.pageSize,
-    filterOpts: [...tableParams.filterOpts, mileageFilter],
+    filterOpts: [...filterOpts, mileageFilter],
     sortOpts: tableParams.sortOpts,
     label,
     level,
@@ -113,7 +114,7 @@ const MetricTable: React.FC<{
     {
       onSuccess: (data) => {
         const items = data.contributorsDetailList.items;
-        const hasTypeFilter = tableParams.filterOpts.find(
+        const hasTypeFilter = filterOpts.find(
           (i) => i.type === 'contribution_type'
         );
         if (hasTypeFilter) {
@@ -149,14 +150,14 @@ const MetricTable: React.FC<{
     for (const key in filters) {
       if (filters.hasOwnProperty(key)) {
         const transformedObj = {
-          type: filterMap[key],
+          type: filterMap[key] || key,
           values: filters[key] as string[],
         };
         filters[key] && filterOpts.push(transformedObj);
       }
     }
     if (filterOpts.find((i) => i.type === 'contribution_type')) {
-      sortOpts = sorter.field && {
+      sortOpts = sorter.order && {
         type:
           sorter.field === 'contribution'
             ? 'contribution_filterd'
@@ -164,14 +165,14 @@ const MetricTable: React.FC<{
         direction: sorter.order === 'ascend' ? 'asc' : 'desc',
       };
     } else {
-      sortOpts = sorter.field && {
+      sortOpts = sorter.order && {
         type: sorter.field,
         direction: sorter.order === 'ascend' ? 'asc' : 'desc',
       };
     }
     handleQueryParams({
-      filterOpts: JSON.stringify(filterOpts),
-      sortOpts: JSON.stringify(sortOpts),
+      filterOpts: filterOpts.length > 0 ? JSON.stringify(filterOpts) : null,
+      sortOpts: sortOpts && JSON.stringify(sortOpts),
     });
     setFilterOpts(filterOpts);
     setTableParams({
@@ -180,7 +181,6 @@ const MetricTable: React.FC<{
         ...pagination,
       },
       sortOpts,
-      filterOpts,
     });
   };
 
@@ -190,10 +190,26 @@ const MetricTable: React.FC<{
       dataIndex: 'contributor',
       align: 'left',
       width: '200px',
-      sorter: true,
       fixed: 'left',
       render: (name) => {
         return <ContributorName name={name} origin={origin} />;
+      },
+      filterIcon: (filtered: boolean) => (
+        <AiOutlineSearch
+          className="text-lg"
+          style={{ color: filtered ? '#1677ff' : undefined }}
+        />
+      ),
+      defaultFilteredValue:
+        defaultFilterOpts.find((i) => i.type === 'contributor')?.values || null,
+      filterDropdown: ({ selectedKeys, setSelectedKeys, confirm }) => {
+        return (
+          <ContributorDropdown
+            selectedKeys={selectedKeys}
+            setSelectedKeys={setSelectedKeys}
+            confirm={confirm}
+          />
+        );
       },
     },
     {
