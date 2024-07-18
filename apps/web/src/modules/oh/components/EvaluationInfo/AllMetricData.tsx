d4a6aba50761d3a2714d90ccbc4c529e24075954
laixingyou <laixy7@gmail.com>
2024-07-18 20:26:43 +0800

    Feat:update report clarification
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- apps/web/src/modules/oh/components/EvaluationInfo/AllMetricData.tsx
+++ apps/web/src/modules/oh/components/EvaluationInfo/AllMetricData.tsx
@@ -31,21 +31,7 @@ export const allMetricData = [
       '设置评分。许可证是 OSI 批准的，且宽松型许可证 10 分，弱宽松型许可证 8 分，非宽松型许可证 6 分；无项目级许可证或许可证不是 OSI 批准的开源许可证 0 分；',
     指标意义: `引入软件许可证合规性检查\n\n【规则】\n1. 禁止选用无许可证、许可证不在准入清单的软件；\n【建议】\n1. 项目的所有源码包含许可头与版权声明；`,
   },
-  {
-    key: 'complianceDco',
-    detailRender: ({ commitCount, commitDcoCount }) => {
-      let res = `引入软件代码提交共有${commitCount}次，其中${commitDcoCount}次已签署 DCO。`;
-      return res;
-    },
-    维度: '合法合规',
-    指标名称: 'DCO',
-    风险重要性: '中',
-    是否必须澄清: '否',
-    指标检查项及评分项:
-      '设置评分。项目所有贡献者签署 DCO 10 分，部分签署 8 分，无人签署 6 分',
-    指标意义:
-      '引入软件代码提交者原创性声明签署检查\n\n【建议】\n1. 项目的提交者签署 DCO；',
-  },
+
   // {
   //   key: 'compliancePackageSig',
   //   detailRender: null,
@@ -75,6 +61,21 @@ export const allMetricData = [
       '设置评分。引入软件项目级、文件级许可证存在兼容性问题 0 分，无兼容性问题 10 分；',
     指标意义:
       '引入软件许可证兼容性检查\n\n【规则】\n1. 禁止引入项目级、文件级 License 存在兼容性问题的软件及版本；',
+  },
+  {
+    key: 'complianceDco',
+    detailRender: ({ commitCount, commitDcoCount }) => {
+      let res = `引入软件代码提交共有${commitCount}次，其中${commitDcoCount}次已签署 DCO。`;
+      return res;
+    },
+    维度: '合法合规',
+    指标名称: 'DCO',
+    风险重要性: '中',
+    是否必须澄清: '否',
+    指标检查项及评分项:
+      '设置评分。项目所有贡献者签署 DCO 10 分，部分签署 8 分，无人签署 6 分',
+    指标意义:
+      '引入软件代码提交者原创性声明签署检查\n\n【建议】\n1. 项目的提交者签署 DCO；',
   },
   {
     key: 'ecologyDependencyAcquisition',
@@ -165,6 +166,18 @@ export const allMetricData = [
     指标意义:
       '引入软件采用度分析，优选在业界广泛应用软件\n\n【建议】\n1. 优选主流的供应商/社区或社区项目；\n2. 优选在业界成熟应用或产品实际使用效果好的软件；',
   },
+
+  {
+    key: 'ecologyPatentRisk',
+    detailRender: null,
+    维度: '技术生态',
+    指标名称: '专利风险',
+    风险重要性: '中',
+    是否必须澄清: '是',
+    指标检查项及评分项: '设置评分，待定',
+    指标意义:
+      '引入软件专利风险分析\n\n【建议】\n1. 优先选择全球专利保护社区 OIN（Open Invention Network）认证软件，未认证软件需单独审视专利风险',
+  },
   {
     key: 'ecologySoftwareQuality',
     detailRender: ({
@@ -190,18 +203,6 @@ export const allMetricData = [
     指标意义:
       '引入软件质量分析，包含代码规范，圈复杂度，代码复用度，测试用例覆盖度\n\n【规则】\n1. 不符合技术架构与技术演进淘汰的软件禁止引入；\n【建议】\n1. 技术架构优选更安全、灵活度高、支持组件化、插件化的软件；\n2. 优选代码质量高的软件，如使用不安全函数数量/密度少、代码结构规范（圈复杂度低）、重复度低、代码调试功能可关闭、有自动化构建能力、自动化测试充分；',
   },
-  {
-    key: 'ecologyPatentRisk',
-    detailRender: null,
-    维度: '技术生态',
-    指标名称: '专利风险',
-    风险重要性: '中',
-    是否必须澄清: '是',
-    指标检查项及评分项: '设置评分，待定',
-    指标意义:
-      '引入软件专利风险分析\n\n【建议】\n1. 优先选择全球专利保护社区 OIN（Open Invention Network）认证软件，未认证软件需单独审视专利风险',
-  },
-
   //   {
   //     key: 'lifecycleVersionNormalization',
   //     detailRender: null,
