27368687d678a014949b819c6f8b787a29489751
laixingyou <laixy7@gmail.com>
2024-07-26 17:34:21 +0800

    Feat:update badge role
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- apps/web/src/modules/oh/components/Report/ReportPage/Approve/CheckApprove.tsx
+++ apps/web/src/modules/oh/components/Report/ReportPage/Approve/CheckApprove.tsx
@@ -92,20 +92,30 @@ const CheckApprove = ({ selectionId }) => {
       ];
     } else {
       let res = [];
+      const leaderState = Boolean(
+        userState?.find((z) => z.memberType === 1 && z.state === 1)
+      );
+      const committerState = Boolean(
+        userState?.find((z) => z.memberType === 0 && z.state === 1)
+      );
       if (commentSigLeadPermission) {
-        const state = Boolean(
-          userState?.find((z) => z.memberType === 1 && z.state === 1)
-        );
+        // const state = Boolean(
+        //   userState?.find((z) => z.memberType === 1 && z.state === 1)
+        // );
         res.push({
-          label: (
+          label: committerState ? (
+            <div className="cursor-not-allowed text-[#d9d9d9]">
+              以 TPC Leader 通过
+            </div>
+          ) : (
             <a
               onClick={() => {
-                handleApprove(1, Number(!state));
+                handleApprove(1, Number(!leaderState));
               }}
             >
               以 TPC Leader 通过
               <span className="ml-2 text-[#3a5bef]">
-                {state && <CheckCircleOutlined rev={undefined} />}
+                {leaderState && <CheckCircleOutlined rev={undefined} />}
               </span>
             </a>
           ),
@@ -113,19 +123,23 @@ const CheckApprove = ({ selectionId }) => {
         });
       }
       if (commentCommitterPermission) {
-        const state = Boolean(
-          userState?.find((z) => z.memberType === 0 && z.state === 1)
-        );
+        // const state = Boolean(
+        //   userState?.find((z) => z.memberType === 0 && z.state === 1)
+        // );
         res.push({
-          label: (
+          label: leaderState ? (
+            <div className="cursor-not-allowed text-[#d9d9d9]">
+              以 Committer 通过
+            </div>
+          ) : (
             <a
               onClick={() => {
-                handleApprove(0, Number(!state));
+                handleApprove(0, Number(!committerState));
               }}
             >
               以 Committer 通过
               <span className="ml-2 text-[#3a5bef]">
-                {state && <CheckCircleOutlined rev={undefined} />}
+                {committerState && <CheckCircleOutlined rev={undefined} />}
               </span>
             </a>
           ),
@@ -155,20 +169,30 @@ const CheckApprove = ({ selectionId }) => {
       ];
     } else {
       let res = [];
+      const leaderState = Boolean(
+        userState?.find((z) => z.memberType === 1 && z.state === -1)
+      );
+      const committerState = Boolean(
+        userState?.find((z) => z.memberType === 0 && z.state === -1)
+      );
       if (commentSigLeadPermission) {
-        const state = Boolean(
-          userState?.find((z) => z.memberType === 1 && z.state === -1)
-        );
+        // const state = Boolean(
+        //   userState?.find((z) => z.memberType === 1 && z.state === -1)
+        // );
         res.push({
-          label: (
+          label: committerState ? (
+            <div className="cursor-not-allowed text-[#d9d9d9]">
+              以 Committer 驳回
+            </div>
+          ) : (
             <a
               onClick={() => {
-                handleApprove(1, state ? 0 : -1);
+                handleApprove(1, leaderState ? 0 : -1);
               }}
             >
               以 TPC Leader 驳回
               <span className="ml-2 text-[#3a5bef]">
-                {state && <CheckCircleOutlined rev={undefined} />}
+                {leaderState && <CheckCircleOutlined rev={undefined} />}
               </span>
             </a>
           ),
@@ -176,19 +200,23 @@ const CheckApprove = ({ selectionId }) => {
         });
       }
       if (commentCommitterPermission) {
-        const state = Boolean(
-          userState?.find((z) => z.memberType === 0 && z.state === -1)
-        );
+        // const state = Boolean(
+        //   userState?.find((z) => z.memberType === 0 && z.state === -1)
+        // );
         res.push({
-          label: (
+          label: leaderState ? (
+            <div className="cursor-not-allowed text-[#d9d9d9]">
+              以 Committer 拒绝
+            </div>
+          ) : (
             <a
               onClick={() => {
-                handleApprove(0, state ? 0 : -1);
+                handleApprove(0, committerState ? 0 : -1);
               }}
             >
               以 Committer 驳回
               <span className="ml-2 text-[#3a5bef]">
-                {state && <CheckCircleOutlined rev={undefined} />}
+                {committerState && <CheckCircleOutlined rev={undefined} />}
               </span>
             </a>
           ),
