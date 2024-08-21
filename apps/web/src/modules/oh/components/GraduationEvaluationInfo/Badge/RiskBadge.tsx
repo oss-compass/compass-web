8082de01b060c2cd7b96adb68951455b1769b99d
laixingyou <laixy7@gmail.com>
2024-08-21 16:57:56 +0800

    fix: role bug
    Signed-off-by: laixingyou <laixy7@gmail.com>

--- apps/web/src/modules/oh/components/GraduationEvaluationInfo/Badge/RiskBadge.tsx
+++ apps/web/src/modules/oh/components/GraduationEvaluationInfo/Badge/RiskBadge.tsx
@@ -176,10 +176,10 @@ const RiskBadge = ({ shortCode, mertic }) => {
       }
     }
   }
-
+  let countGreen = createBadge('', '#52c41a', '', count);
   return (
     <div className="flex w-8 flex-shrink-0 items-center justify-center">
-      {BadgeContent}
+      {needClarification ? BadgeContent : ''}
     </div>
   );
 };
