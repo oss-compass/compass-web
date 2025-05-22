8ef45076f489a48eb94063cb28f1b06eed73012e
coder-sett <laixy7@gmail.com>
2025-05-22 14:56:10 +0800

    feat: update lab
    Signed-off-by: coder-sett <laixy7@gmail.com>

--- apps/web/src/modules/lab/model/Detail/index.tsx
+++ apps/web/src/modules/lab/model/Detail/index.tsx
@@ -2,35 +2,25 @@ import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Center from '@common/components/Layout/Center';
 import { useTranslation } from 'react-i18next';
-import { formState, actions } from '../Form/state';
+import { IoArrowBack } from 'react-icons/io5';
 import { useLabModelDetail } from '../hooks';
 import { useEventEmitter } from 'ahooks';
 import { ReFetch } from '@common/constant';
 import DetailPage from './DetailPage';
+import Link from 'next/link';
 
 const ModelDetail = () => {
   const router = useRouter();
   const { t } = useTranslation();
   const modelId = Number(router.query.model);
   const { data: modelDetail, isLoading, refetch } = useLabModelDetail();
-
+  console.log(modelDetail);
   const event$ = useEventEmitter<string>();
   event$.useSubscription((flag) => {
     if (flag === ReFetch) {
       refetch();
     }
   });
-  // useEffect(() => {
-  //   actions.resetForm();
-  //   if (modelDetail?.labModelDetail) {
-  //     const { name, dimension, isGeneral, isPublic } =
-  //       modelDetail.labModelDetail;
-  //     formState.name = name;
-  //     formState.dimension = dimension;
-  //     formState.isGeneral = isGeneral;
-  //     formState.isPublic = isPublic;
-  //   }
-  // }, [modelDetail]);
 
   const getContent = () => {
     if (isLoading) {
@@ -64,8 +54,17 @@ const ModelDetail = () => {
   return (
     <div className="flex-1 bg-[#ffffff] py-12 text-sm">
       <Center className="my-auto md:px-4">
+        <div className="hover:text-primary text-lg">
+          <Link
+            href={`/lab?modelType=${
+              modelDetail?.labModelDetail?.modelType || '0'
+            }`}
+            className="flex items-center gap-2 px-4"
+          >
+            <IoArrowBack /> {t('common:btn.back')}
+          </Link>
+        </div>
         <div className="mb-6 flex items-center justify-between">
-          {/* <div className="text-xl font-semibold"></div> */}
           {getContent()}
         </div>
       </Center>
