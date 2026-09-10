75d90fc41a74c8ceaa68592d6db0d25779e44e8d
coder-sett <laixy7@gmail.com>
2026-09-10 20:28:56 +0800

    feat: update issue report
    Signed-off-by: coder-sett <laixy7@gmail.com>

--- apps/web/src/modules/intelligent-analysis/DeveloperExperience/IssueContribution/components/IssueOverview/index.tsx
+++ apps/web/src/modules/intelligent-analysis/DeveloperExperience/IssueContribution/components/IssueOverview/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { useQuery } from '@tanstack/react-query';
-import { Button, Empty, Skeleton, Tooltip, Typography } from 'antd';
-import { HistoryOutlined, RightOutlined } from '@ant-design/icons';
+import { Empty, Skeleton, Typography } from 'antd';
+import { RightOutlined } from '@ant-design/icons';
 import { buildIssueRepoManagementHref } from '../../../routes';
 import IssueTrendSparkline from './IssueTrendSparkline';
 import { fetchIssueOverview, fetchIssueTopPains } from '../../data';
@@ -11,7 +11,6 @@ import { computeIssueOverview, latestReposByPeriod } from './issueMetrics';
 import IssueTrendModal from './IssueTrendModal';
 import IssueRepoProgressSection from './IssueRepoProgressSection';
 import IssueMetricsAppendix from './IssueMetricsAppendix';
-import RerunJobListModal from '../RerunJobListModal';
 import {
   IssueRepoRankings,
   IssueScoreDistribution,
@@ -44,7 +43,6 @@ const IssueOverview: React.FC<IssueOverviewProps> = ({ org }) => {
   const [appendixOpen, setAppendixOpen] = React.useState(false);
   const [trendModal, setTrendModal] =
     React.useState<IssueTrendModalData | null>(null);
-  const [rerunListOpen, setRerunListOpen] = React.useState(false);
   const repoManagementHref = buildIssueRepoManagementHref({ org });
 
   const {
@@ -192,21 +190,9 @@ const IssueOverview: React.FC<IssueOverviewProps> = ({ org }) => {
 
   return (
     <>
-      <div className="flex items-center justify-between gap-3">
-        <Title level={4} className="oj-section-title">
-          总览信息
-        </Title>
-        <Tooltip title="查看当前组织全部仓库的 Issue 复测任务">
-          <Button
-            size="small"
-            className="!rounded-lg"
-            icon={<HistoryOutlined />}
-            onClick={() => setRerunListOpen(true)}
-          >
-            重跑任务
-          </Button>
-        </Tooltip>
-      </div>
+      <Title level={4} className="oj-section-title">
+        总览信息
+      </Title>
       <div
         className="overview-bottom-row"
         style={{ gridTemplateColumns: 'repeat(4, minmax(0, 1fr))' }}
@@ -338,11 +324,6 @@ const IssueOverview: React.FC<IssueOverviewProps> = ({ org }) => {
         open={!!trendModal}
         trend={trendModal}
         onClose={() => setTrendModal(null)}
-      />
-      <RerunJobListModal
-        open={rerunListOpen}
-        onClose={() => setRerunListOpen(false)}
-        scope={{ type: 'org', org }}
       />
     </>
   );
